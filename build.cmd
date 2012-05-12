4833ab37dd57c7751873e56a4f2c8e7e33568fe3
Colin Clark <colinbdclark@gmail.com>
2012-05-12 01:27:17 -0400

    Tweaks to windows build script.

--- build.cmd
+++ build.cmd
@@ -3,3 +3,6 @@ mkdir bin
 copy lib\libcurl.dll bin
 start build.cmd
 cd ..
+
+if not exist ../node_modules mkdir ../node_modules
+if not exist ../node_modules/universal git clone git://github.com/GPII/universal.git
