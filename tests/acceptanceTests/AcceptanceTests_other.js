f64069a5a15494156b8621d20fc7eb2e315e145f
kaspermarkus <kasper2@markus.dk>
2014-03-18 17:47:37 +0100

    GPII-641: Added acceptance tests for the default device reporter file - checking that the built-in applications work and that at least 1 3rd party application doesn't

--- tests/acceptanceTests/AcceptanceTests_other.js
+++ tests/acceptanceTests/AcceptanceTests_other.js
@@ -22,11 +22,166 @@ fluid.require("./AcceptanceTests_include", require);
 
 var testDefs = [
     {
-        name: "Testing that the system doesn't crashes on login with empty NP set",
-        token: "empty",
+    //     name: "Testing that the system doesn't crashes on login with empty NP set",
+    //     token: "empty",
+    //     settingsHandlers: {},
+    //     processes: []
+    // }, {
+        name: "Default device reporter with screenreader_nvda NP set - NVDA is not expected launched",
+        token: "screenreader_nvda",
         settingsHandlers: {},
-        processes: []
+        processes: [
+            {
+                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq nvda.exe\" | find /I \"nvda.exe\" /C",
+                "expectConfigured": "0",
+                "expectRestored": "0"
+            }
+        ]
+    }, {
+        name: "Testing os_win7 using Flat matchmaker with default device reporter",
+        token: "os_win7",
+        settingsHandlers: {
+            "gpii.windows.spiSettingsHandler": {
+                "data": [
+                    {
+                        "settings": {
+                            "MouseTrails": {
+                                "path": {
+                                    "get": "pvParam",
+                                    "set": "uiParam"
+                                },
+                                "value": 10
+                            }
+                        },
+                        "options": {
+                            "getAction": "SPI_GETMOUSETRAILS",
+                            "setAction": "SPI_SETMOUSETRAILS",
+                            "uiParam": 0,
+                            "pvParam": {
+                                "type": "BOOL"
+                            }
+                        }
+                    }, { //high contrast settings
+                        "settings": {
+                            "HighContrastOn": {
+                                "path": "pvParam.dwFlags.HCF_HIGHCONTRASTON",
+                                "value": true
+                            }
+                        },
+                        "options": {
+                            "getAction": "SPI_GETHIGHCONTRAST",
+                            "setAction": "SPI_SETHIGHCONTRAST",
+                            "uiParam": "struct_size",
+                            "pvParam": {
+                                "type": "struct",
+                                "name": "HIGHCONTRAST"
+                            }
+                        }
+                    }
+                ]
+           },
+            "gpii.windows.registrySettingsHandler": {
+                "data": [{ //magnifier stuff
+                    "settings": {
+                        "Invert": {
+                            "dataType": "REG_DWORD",
+                            "value": 1
+                        },
+                        "Magnification": {
+                            "dataType": "REG_DWORD",
+                            "value": 150
+                        },
+                        "MagnificationMode": {
+                            "dataType": "REG_DWORD",
+                            "value": 3
+                        },
+                        "FollowFocus": {
+                            "value": 0,
+                            "dataType": "REG_DWORD"
+                        },
+                        "FollowCaret": {
+                            "value": 1,
+                            "dataType": "REG_DWORD"
+                        },
+                        "FollowMouse": {
+                            "value": 1,
+                            "dataType": "REG_DWORD"
+                        }
+                    },
+                    "options": {
+                        "hKey": "HKEY_CURRENT_USER",
+                        "path": "Software\\Microsoft\\ScreenMagnifier"
+                    }
+                }, { //cursor size stuff
+                    "settings": {
+                        "No": {
+                            "value": "%SystemRoot%\\cursors\\aero_unavail_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "Hand": {
+                            "value": "%SystemRoot%\\cursors\\aero_link_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "Help": {
+                            "value": "%SystemRoot%\\cursors\\aero_helpsel_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "Wait": {
+                            "value": "%SystemRoot%\\cursors\\aero_busy_xl.ani",
+                            "dataType": "REG_SZ"
+                        },
+                        "Arrow": {
+                            "value": "%SystemRoot%\\cursors\\aero_arrow_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "NWPen": {
+                            "value": "%SystemRoot%\\cursors\\aero_pen_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "SizeNS": {
+                            "value": "%SystemRoot%\\cursors\\aero_ns_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "SizeWE": {
+                            "value": "%SystemRoot%\\cursors\\aero_ew_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "SizeAll": {
+                            "value": "%SystemRoot%\\cursors\\aero_move_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "UpArrow": {
+                            "value": "%SystemRoot%\\cursors\\aero_up_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "SizeNESW": {
+                            "value": "%SystemRoot%\\cursors\\aero_nesw_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "SizeNWSE": {
+                            "value": "%SystemRoot%\\cursors\\aero_nwse_xl.cur",
+                            "dataType": "REG_SZ"
+                        },
+                        "AppStarting": {
+                            "value": "%SystemRoot%\\cursors\\aero_working_xl.ani",
+                            "dataType": "REG_SZ"
+                        }
+                    },
+                    "options": {
+                        "hKey": "HKEY_CURRENT_USER",
+                        "path": "Control Panel\\Cursors"
+                    }
+                }]
+            }
+        },
+        processes: [
+            {
+                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq Magnify.exe\" | find /I \"Magnify.exe\" /C",
+                "expectConfigured": "1",
+                "expectRestored": "0"
+            }
+        ]
     }
 ];
 
-gpii.acceptanceTesting.windows.runTests("builtIn_config", testDefs);
+gpii.acceptanceTesting.windows.runTests("standard_development", testDefs);
