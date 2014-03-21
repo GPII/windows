25badec731c2b249b357c9e007656f5f61f2dddb
cstrobbe <c_strobbe-github@yahoo.co.uk>
2014-03-21 14:07:12 +0100

    GPII-288: Log settings for current token(s)
    This is the second part of several pull requests for ticket GPII-288. The Node.js part is in a pull request for the pilots2 branch of the universal repository: https://github.com/GPII/universal/pull/239.

---
+++ LogLocalSettings.cmd
@@ -0,0 +1,6 @@
+@echo off
+rem You can change the location where the settings logs are saved by replacing 
+rem 'C:\gpiilogs_pilot2' in the following line by another location. Don't use a location containing spaces!!
+@echo on
+node ../node_modules/universal/gpii/node_modules/matchMaker/src/LogSettings.js D:\gpiilogs_pilot2
+@echo off
