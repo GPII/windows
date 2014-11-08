0bad71ee94ad75290049188cb5710ad601b4f821
Antranig Basman <antranig.basman@colorado.edu>
2014-11-08 17:28:19 +0000

    GPII-434: Fixed SPI Settings Handler unit tests so that they no longer corrupt the desktop. Improvements to commenting and logging

--- build.cmd
+++ build.cmd
@@ -1,3 +1,6 @@
+rem This script should be run from its own directory
+rem It requires MinGW to be installed - see instructions in USBUserListener readme
+
 cd UsbUserListener
 mkdir bin
 copy lib\libcurl.dll bin
