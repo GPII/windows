fcb8f5c9e2c255194934ea1f0cdf5e64e9e38362
Avtar Gill <avtargill@gmail.com>
2016-05-28 10:41:42 -0400

    GPII-1814: Moves npm command to related script

--- provisioning/chocolatey-packages.bat
+++ provisioning/chocolatey-packages.bat
@@ -1,4 +1,3 @@
 choco install nodejs.install -version 4.4.3 -y
 choco install python2 -y
 setx /M PATH "%PATH%;C:\Program Files\nodejs;C:\tools\python2"
-call npm config set msvs_version 2015 --global
