806355e3e6dae4d81f322f95de1487f50cd5c5ba
Avtar Gill <avtargill@gmail.com>
2016-06-01 15:06:29 -0400

    GPII-1814: Replaces gpii-universal Grunt task with batch file
    Using the gpii-universal Grunt task on a Windows 10 VM using Vagrant
    file shares results in "invalid symlink" "function not implemented"
    errors. This change is a workaround for that issue.

--- provisioning/build.bat
+++ provisioning/build.bat
@@ -1,3 +1,10 @@
 cd c:\vagrant
 npm install
-grunt build
+
+mkdir c:\node_modules
+cd c:\node_modules
+git clone --depth 1 https://github.com/GPII/universal.git
+cd universal
+npm install
+npm install dedupe-infusion
+node -e "require('dedupe-infusion')()"
