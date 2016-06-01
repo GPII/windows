79048a9716ff89bf49dde5be1c4fcde92cacccc2
Avtar Gill <avtargill@gmail.com>
2016-06-01 17:01:01 -0400

    GPII-1814: Moves Universal related tasks to separate batch file

--- provisioning/build.bat
+++ provisioning/build.bat
@@ -1,10 +1,3 @@
 cd c:\vagrant
 npm install
 
-mkdir c:\node_modules
-cd c:\node_modules
-git clone --depth 1 https://github.com/GPII/universal.git
-cd universal
-npm install
-npm install dedupe-infusion
-node -e "require('dedupe-infusion')()"
