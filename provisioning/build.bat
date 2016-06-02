cc180918ff09efc7b96f475479f4afc805432a45
Nikos Minogiannis <nminogiannis@singularlogic.eu>
2016-06-02 15:35:18 +0300

    GPII-1816: Include USB/RFID listeners in CI Build

--- provisioning/build.bat
+++ provisioning/build.bat
@@ -1,3 +1,8 @@
 cd c:\vagrant
-npm install
+call npm install
+
+call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools_msbuild.bat"
+cd c:\vagrant\listeners
+msbuild listeners.sln /p:Configuration=Release /p:FrameworkPathOverride="C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1"
+cd c:\vagrant
 
--- provisioning/chocolatey-packages.bat
+++ provisioning/chocolatey-packages.bat
@@ -1,3 +1,4 @@
 choco install nodejs.install -version 4.4.3 -y
 choco install python2 -y
-setx /M PATH "%PATH%;C:\Program Files\nodejs;C:\tools\python2"
+choco install innosetup -y
+setx /M PATH "%PATH%;C:\Program Files\nodejs;C:\tools\python2;C:\Program Files (x86)\Inno Setup 5"
