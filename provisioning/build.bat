32d7eccacfa5509ef90d9543627e82c02de5d361
Avtar Gill <avtargill@gmail.com>
2016-06-16 00:59:47 -0400

    GPII-1826: Switches Node.js to 32-bit version

--- provisioning/build.bat
+++ provisioning/build.bat
@@ -1,3 +1,11 @@
 cd c:\vagrant
-npm install
+call npm install
+
+mkdir c:\node_modules
+cd c:\node_modules
+git clone --depth 1 https://github.com/GPII/universal.git
+cd universal
+call npm install
+call npm install dedupe-infusion
+node -e "require('dedupe-infusion')()"
 
