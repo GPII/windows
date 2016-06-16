57eacb7ce60c46dc7cd9552b94003d2ef3d31b1f
Avtar Gill <avtargill@gmail.com>
2016-06-16 10:03:00 -0400

    GPII-1826: Removes unnecessary deduping step

--- provisioning/build.bat
+++ provisioning/build.bat
@@ -6,6 +6,4 @@ cd c:\node_modules
 git clone --depth 1 https://github.com/GPII/universal.git
 cd universal
 call npm install
-call npm install dedupe-infusion
-node -e "require('dedupe-infusion')()"
 
