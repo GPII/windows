9499669243a9b3c9cff1d1dc62fc706288d1ba47
Javier Hernández <javi@raisingthefloor.org>
2016-08-03 14:07:16 +0200

    GPII-1000: Fixed the tests

--- tests/UnitTests.js
+++ tests/UnitTests.js
@@ -3,6 +3,7 @@
 GPII Unit Testing for Windows
 
 Copyright 2014 Lucendo Development Ltd.
+Copyright 2016 Raising the Floor - US
 
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
