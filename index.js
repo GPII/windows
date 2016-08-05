aacb47f27a35e5871dde70f6a412e7c909c1551e
Joseph Scheuhammer <clown@alum.mit.edu>
2016-08-05 14:11:21 -0400

    GPII-442:  Windows: Load process reporter module.

--- index.js
+++ index.js
@@ -24,5 +24,6 @@ require("./gpii/node_modules/killProcess/killProcess.js");
 require("./gpii/node_modules/registrySettingsHandler");
 require("./gpii/node_modules/registryResolver");
 require("./gpii/node_modules/spiSettingsHandler");
+require("./gpii/node_modules/processReporter");
 
 module.exports = fluid;
