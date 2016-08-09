5d4a7dd5702d9973bb837f31d1b75d347e552a19
Javier Hernández <javi@raisingthefloor.org>
2016-08-09 12:48:00 +0200

    GPII-1898: Update to node 6

--- provisioning/chocolatey-packages.bat
+++ provisioning/chocolatey-packages.bat
@@ -1,4 +1,4 @@
-choco install nodejs.install -version 4.4.3 --forcex86 -y
+choco install nodejs.install -version 6.3.1 --forcex86 -y
 choco install python2 -y
 choco install innosetup -y
 setx /M PATH "%PATH%;C:\Program Files (x86)\nodejs;C:\tools\python2;C:\Program Files (x86)\Inno Setup 5"
