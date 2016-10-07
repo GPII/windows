548366923082276329f466428118869d7fe5783c
Alejandro Leiva <gloob@litio.org>
2016-10-07 11:25:58 +0200

    GPII-1993 : Adding /nodeReuse:false to spawn a new process always for compiling the artifacts.

--- provisioning/Build.ps1
+++ provisioning/Build.ps1
@@ -30,7 +30,7 @@ Invoke-Environment "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbu
 
 $msbuild = Get-MSBuild "4.0"
 $listenersDir = Join-Path $mainDir "listeners"
-Invoke-Command $msbuild "listeners.sln /p:Configuration=Release /p:FrameworkPathOverride=`"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $listenersDir
+Invoke-Command $msbuild "listeners.sln /nodeReuse:false /p:Configuration=Release /p:FrameworkPathOverride=`"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $listenersDir
 
 Invoke-Environment "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat"
 
