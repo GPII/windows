b06be70c49d1b9d63fe08542aee7177ad2b34413
Joseph Scheuhammer <clown@alum.mit.edu>
2017-08-02 11:55:53 -0400

    GPII-1939: New Platform Reporter reports screen resolutions
    Added machinery for providing a Linux/GNOME Platform Reporter that
    uses the Windows display settings handler to report all screen resolutions.
    
    Note:  includes code from pull request for GPII-2404, "Windows display
    settings handler should report all available screen resolutions".

--- tests/UnitTests.js
+++ tests/UnitTests.js
@@ -20,3 +20,4 @@ require("../gpii/node_modules/spiSettingsHandler/test/testSpiSettingsHandler.js"
 require("../gpii/node_modules/processHandling/test/testProcessHandling");
 require("../gpii/node_modules/registryResolver/test/testRegistryResolver.js");
 require("../gpii/node_modules/registeredAT/test/testRegisteredAT.js");
+require("../gpii/node_modules/platformReporter/test/PlatformReporterTests.js");
