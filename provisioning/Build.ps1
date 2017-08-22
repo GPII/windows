b89fab41beb87297ef53417ccf639511ee0bd871
Alejandro Leiva <gloob@litio.org>
2017-08-22 17:42:53 +0200

    Explicitely add Platform in the recipe of the listeners compilation.

--- provisioning/Build.ps1
+++ provisioning/Build.ps1
@@ -38,7 +38,7 @@ if (-Not $skipNpm) {
 Invoke-Environment "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools_msbuild.bat"
 $msbuild = Get-MSBuild "4.0"
 $listenersDir = Join-Path $mainDir "listeners"
-Invoke-Command $msbuild "listeners.sln /nodeReuse:false /p:Configuration=Release /p:FrameworkPathOverride=`"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $listenersDir
+Invoke-Command $msbuild "listeners.sln /nodeReuse:false /p:Configuration=Release /p:Platform=`"Win32`" /p:FrameworkPathOverride=`"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $listenersDir
 
 
 $msbuild = Get-MSBuild "4.0"
