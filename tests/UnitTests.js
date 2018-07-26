987c12eac071c4e151169927ae0b92013d43862d
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2018-07-26 13:15:48 +0200

    GPII-3145: Fixed reference to new test file in UnitTests.js

--- tests/UnitTests.js
+++ tests/UnitTests.js
@@ -23,5 +23,5 @@ require("../gpii/node_modules/registeredAT/test/testRegisteredAT.js");
 require("../gpii/node_modules/windowsMetrics/test/WindowsMetricsTests.js");
 require("../gpii/node_modules/processReporter/test/all-tests.js");
 require("../gpii/node_modules/windowMessages/test/windowMessagesTest.js");
-require("../gpii/node_modules/windowMessages/test/nativeSettingsHandler.js");
+require("../gpii/node_modules/nativeSettingsHandler/test/testNativeSettingsHandler.js");
 require("../gpii/node_modules/userListeners/test/all-tests.js");
