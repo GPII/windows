51c2d8898b7768cc8ab2bc5a5ccb50fd0af04fe9
ste <git@grundell.co.uk>
2018-09-11 12:38:01 +0100

    GPII-2971: Renamed IoD module name to gpii-iod

--- index.js
+++ index.js
@@ -44,6 +44,6 @@ require("./gpii/node_modules/userListeners");
 require("./gpii/node_modules/systemSettingsHandler");
 require("./gpii/node_modules/gpii-app-zoom");
 require("./gpii/node_modules/gpii-service-handler");
-require("./gpii/node_modules/installOnDemand");
+require("./gpii/node_modules/gpii-iod");
 
 module.exports = fluid;
