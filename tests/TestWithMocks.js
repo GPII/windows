6988ee69066550039164fee9353e3c81d14db129
Bryan Young <bryan@raisingthefloor.org>
2019-01-18 16:17:01 -0800

    GPII-3596 refactor mocked methods into utility

--- tests/TestWithMocks.js
+++ tests/TestWithMocks.js
@@ -17,27 +17,24 @@
 
 "use strict";
 
-var fluid = require("gpii-universal"),
-    path = require("path");
-
-var gpii = fluid.registerNamespace("gpii");
 var windows = fluid.registerNamespace("gpii.windows");
 fluid.registerNamespace("gpii.windows.tests");
 var _ = require("lodash");
 
 windows.tests.testWithMocks = function (mockedMethods, test, fail) {
     var restore = [];
-    for(var i=0; i < mockedMethods.length; i++) {
+    var i;
+    for (i = 0; i < mockedMethods.length; i++) {
         restore[i] = _.get(mockedMethods[i].original.obj, mockedMethods[i].original.path);
         _.set(mockedMethods[i].original.obj, mockedMethods[i].original.path, mockedMethods[i].mock);
     }
     try {
         test();
-    } catch(e) {
+    } catch (e) {
         fail(e);
     } finally {
-        for(var i=0; i < mockedMethods.length; i++) {
+        for (i = 0; i < mockedMethods.length; i++) {
             _.set(mockedMethods[i].original.obj, mockedMethods[i].original.path, restore[i]);
         }
     }
-}
+};
