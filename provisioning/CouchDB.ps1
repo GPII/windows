c05aee243b5cea54650d2cb06696547980d3e91c
Javier Hernández <javi@raisingthefloor.org>
2019-02-22 11:53:01 +0100

    GPII-3744: Updating the default listening port

--- provisioning/CouchDB.ps1
+++ provisioning/CouchDB.ps1
@@ -9,9 +9,9 @@ param ( # default to script path if no parameter is given
     [string]$originalBuildScriptPath = (Split-Path -parent $PSCommandPath)
 )
 
-Write-OutPut "Adding CouchDB to the system"
 Import-Module "$($originalBuildScriptPath)/Provisioning.psm1" -Force
 
+#Write-OutPut "Adding CouchDB to the system"
 $couchDBInstallerURL = "http://archive.apache.org/dist/couchdb/binary/win/2.3.0/couchdb-2.3.0.msi"
 $couchDBInstaller = Join-Path $originalBuildScriptPath "couchdb-2.3.0.msi"
 
@@ -47,13 +47,14 @@ try {
 }
 
 # Replace the default listening port
-# By default, CouchDB will be installed at C:\CouchDB and we need admin privileges.
-#$couchDBConfigFile = Join-Path (Join-Path "C:\CouchDB" "etc") "default.ini"
-#((Get-Content -path $couchDBConfigFile -Raw) -replace "5984","25984") | Set-Content -Path $couchDBConfigFile
-# In addition to that, we must restart CouchDB in order for the changes to take effect.
-#$net = "net.exe"
-#Invoke-Command $net "stop 'Apache CouchDB'"
-#Invoke-Command $net "start 'Apache CouchDB'"
+# By default, CouchDB will be installed at C:\CouchDB.
+Write-OutPut "Changing default listening port to 25984 ..."
+$couchDBConfigFile = Join-Path (Join-Path "C:\CouchDB" "etc") "default.ini"
+((Get-Content -path $couchDBConfigFile -Raw) -replace "5984","25984") | Set-Content -Path $couchDBConfigFile
+
+# In addition to that, we must restart CouchDB in order for the changes to take effect
+Write-OutPut "Restarting CouchDB ..."
+Restart-Service -Name "Apache CouchDB"
 
 Write-OutPut "CouchDB is now installed and configured"
 exit 0
