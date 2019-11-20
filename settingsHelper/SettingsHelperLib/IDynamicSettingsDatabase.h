b1a7b80ebd86d4275301f73ac8b7904fe970f2a4
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2019-11-20 17:41:28 +0100

    GPII-4152: Changed comment style to reflect the fact that is research info

--- settingsHelper/SettingsHelperLib/IDynamicSettingsDatabase.h
+++ settingsHelper/SettingsHelperLib/IDynamicSettingsDatabase.h
@@ -17,7 +17,12 @@
 #include <roapi.h>
 #include <windows.foundation.h>
 
-// SystemSettings::NotificationsDataModel::AppSettingsDynamicDatabase::GetSetting(struct HSTRING__ *,struct SystemSettings::DataModel::ISettingItem * *)
+/**
+ * RESEARCH DATA: This is the RAW pointer table found for the interface IDynamicSettingsDatabase
+ * ============================================================================
+ *
+ * 6 -> SystemSettings::NotificationsDataModel::AppSettingsDynamicDatabase::GetSetting(struct HSTRING__ *,struct SystemSettings::DataModel::ISettingItem * *)
+ */
 
 __interface __declspec(uuid("2bea7562-66a9-47a1-aebd-aa188e3a1b57"))
 IDynamicSettingsDatabase : public IInspectable {
