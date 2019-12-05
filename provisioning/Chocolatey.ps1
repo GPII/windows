1a402028bd87bf722ded4f668d2e515ac64e6953
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2019-12-05 19:48:54 +0100

    GPII-4152: Temporal fix reinstalling base packages for building native dependencies

--- provisioning/Chocolatey.ps1
+++ provisioning/Chocolatey.ps1
@@ -36,11 +36,23 @@ refreshenv
 Invoke-Command $chocolatey "uninstall windows-sdk-8.1 -y"
 refreshenv
 
+Invoke-Command $chocolatey "uninstall microsoft-build-tools -y"
+refreshenv
+
+Invoke-Command $chocolatey "uninstall visualcppbuildtools -y"
+refreshenv
+
 # Install the required ATL Library & WindowsSDK for SystemSettingsHandler
 Invoke-Command $chocolatey 'install --force -y vcbuildtools -ia "/InstallSelectableItems VisualCppBuildTools_ATLMFC_SDK;VisualCppBuildTools_NETFX_SDK"'
 refreshenv
 
 Invoke-Command $chocolatey "install windows-sdk-10-version-1809-all --version=10.0.17763.1 -y"
+refreshenv
+
+Invoke-Command $chocolatey "install microsoft-build-tools -y"
+refreshenv
+
+Invoke-Command $chocolatey "install visualcppbuildtools -y"
 refreshenv
 
 exit 0
